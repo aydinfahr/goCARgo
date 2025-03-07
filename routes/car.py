eb180a52db55153c79931e6adea0427cfbd6eb52
Fahrettin Aydin <aydn.fhr@gmail.com>
2025-03-07 12:16:04 +0100

    Add enums for seat numbers and ride status; refactor car and ride routes for improved functionality

--- routes/car.py
+++ routes/car.py
@@ -1,8 +1,6 @@
+from typing import Optional
 from sqlalchemy.orm import Session            
-
-
 from fastapi import APIRouter, Depends
-
 from db import db_car
 from db.database import get_db
 from schemas import CarBase, CarDisplay
@@ -13,28 +11,27 @@ router = APIRouter(
     tags=['car']
 )
 
-
 # Create car
-@router.post('/{owner_id}', response_model=CarDisplay)
-def create_car(owner_id : int, request: CarBase, db: Session=Depends(get_db)):
-    return db_car.create_car(owner_id, db, request)
-
-# Get Car Details
-@router.get("/{car_id}", response_model=CarDisplay)
-def get_car(car_id: int, db: Session = Depends(get_db)):
-    return db_car.get_car_by_id(db, car_id)
+@router.post('/', response_model=CarDisplay)
+def create_car(request: CarBase, db: Session=Depends(get_db)):
+    return db_car.create_car(db, request)
 
 # List User's Cars
-@router.get("/owner/{owner_id}", response_model=list[CarDisplay])
-def get_user_cars(owner_id: int, db: Session = Depends(get_db)):
-    return db_car.get_car(db, owner_id)
+@router.get("/", response_model=list[CarDisplay])
+def get_all_cars(db: Session = Depends(get_db), owner_id: Optional[int]=None):  #? db: Session = Depends(get_db)'i db_car'da mi tanimlasak
+    return db_car.get_all_cars(db, owner_id)
+
+# Get Car Details
+@router.get("/{id}", response_model=CarDisplay)
+def get_car(id: int, db: Session = Depends(get_db)):
+    return db_car.get_car_by_id(db, id)
 
 # Update Car Details
-@router.put("/edit/{car_id}", response_model=CarDisplay)
-def update_car(car_id: int, car_data: CarBase, db: Session = Depends(get_db)):
-    return db_car.update_car(db,car_id, car_data)
+@router.put("/{id}", response_model=CarDisplay) 
+def update_car(id: int, request: CarBase, db: Session = Depends(get_db)):
+    return db_car.update_car(db, id, request)
 
 # Delete Car
-@router.delete("/{car_id}", response_model=CarDisplay)
-def delete_car(car_id: int, db: Session = Depends(get_db)):
-    return db_car.delete_car(db, car_id)
+@router.delete("/{id}")
+def delete_car(id: int, db: Session = Depends(get_db), owner_id: int=None, ): #? Owner_id Query Param olarak mi alinmali?
+    return db_car.delete_car(db, id, owner_id)
